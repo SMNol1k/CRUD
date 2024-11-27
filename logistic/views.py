7152c4f46e333209f72dc71b379c60cc4b60b47e
Maxim <roqueknight123456@gmail.com>
2024-11-27 13:52:41 +0300

    Deploy Server4

--- logistic/views.py
+++ logistic/views.py
@@ -28,4 +28,4 @@ class StockViewSet(viewsets.ModelViewSet):
 
 @api_view(['GET'])
 def sample_view(request):
-    return Response({'message': 'ПРИВЕТ ceeeb!!!!!'})
+    return Response({'message': 'ПРИВЕТ asdfads!!!!!'})
